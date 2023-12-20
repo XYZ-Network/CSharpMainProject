17a874074c6f4ed61ab8ad62e102ffe295f2f1f7
Данила <danila@Ulanas-MacBook-Pro.local>
2023-12-20 17:01:29 +0200

    Comment

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -10,7 +10,14 @@ public class EnterPoint : MonoBehaviour
 {
     [SerializeField] private Settings _settings;
     [SerializeField] private Canvas _targetCanvas;
-    
+
+    // Комментарий
+
+    /*/
+     * dsfasga
+     * sdasdfgads
+    /*/
+
     void Start()
     {
         _settings.LoadPrefabs();
