f1cacdac67ad7817c4cd66edfa3eb92642a6a82a
ckupid <barysnikovaleksej08@gmail.com>
2023-12-28 23:39:54 +0300

    dz

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -10,13 +10,10 @@ public class EnterPoint : MonoBehaviour
 {
     [SerializeField] private Settings _settings;
     [SerializeField] private Canvas _targetCanvas;
-    private float _timeScale = 1;
+    private float _timeScale = 5;
     //hhhhh
-    /*
-     * 
-     * bebra
-     * 
-     */
+     
+     
     void Start()
     {
         Time.timeScale = _timeScale;
