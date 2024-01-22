e2895e4fa47de9bd9092e19e1cce1ffe1d24fdbb
Grendidg <sillenthilenok@rambler.ru>
2024-01-22 21:48:56 +0300

    2dz

--- Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
+++ Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
@@ -4,7 +4,7 @@ namespace Model.Runtime.Projectiles
 {
     public class ArchToTileProjectile : BaseProjectile
     {
-        private const float ProjectileSpeed =1.3f;
+        private const float ProjectileSpeed =1.4f;
         private readonly Vector2Int _target;
         private readonly float _timeToTarget;
         private readonly float _totalDistance;
