42706b3c905a10e4e5aa3504fb5852c2f00a6389
Max <>
2024-01-31 11:30:06 +0100

    ДЗ6_Maxangar

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Model;
 using Model.Runtime.Projectiles;
 using UnityEngine;
 
@@ -12,38 +14,107 @@ namespace UnitBrains.Player
         private float _temperature = 0f;
         private float _cooldownTime = 0f;
         private bool _overheated;
-        
+        private List<Vector2Int> outOfRangeTargets = new List<Vector2Int>(); //список целей, которые вне досягаемости
+
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
+
             float overheatTemperature = OverheatTemperature;
-            ///////////////////////////////////////
-            // Homework 1.3 (1st block, 3rd module)
-            ///////////////////////////////////////           
-            var projectile = CreateProjectile(forTarget);
-            AddProjectileToList(projectile, intoList);
-            ///////////////////////////////////////
+            float currenttemp = GetTemperature();
+
+            if (currenttemp >= overheatTemperature)
+            {
+                return;
+            }
+
+            IncreaseTemperature();
+
+            for (int i = 0; i <= currenttemp; i++)
+            {
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
+            }
+
         }
+
 
         protected override List<Vector2Int> SelectTargets()
         {
-            ///////////////////////////////////////
-            // Homework 1.4 (1st block, 4rd module)
-            ///////////////////////////////////////
-            List<Vector2Int> result = GetReachableTargets();
-            while (result.Count > 1)
+
+            Vector2Int minTarget = Vector2Int.zero;
+            float min = float.MaxValue;
+
+            List<Vector2Int> result = GetAllTargets().ToList();
+
+            if (result.Count > 0) //проверяем, есть ли хотя бы одна цель и если есть - определяем ближайшую
             {
-                result.RemoveAt(result.Count - 1);
+                foreach (Vector2Int target in GetAllTargets())
+                {
+
+                    float DistanceToBase = DistanceToOwnBase(target);
+
+                    if (DistanceToBase < min)
+                    {
+                        minTarget = target;
+
+                        if (IsTargetInRange(target)) //если цель в зоне досягаемости - добавляем в result
+                        {
+                            result.Add(minTarget);
+                        }
+
+                        outOfRangeTargets.Add(target); //если цель вне зоны досягаемости, записываем ее в список outOfRangeTargets
+                    }
+                }
+                
+
             }
+            else //добавляем в цели базу противника если целей нету в списке всех целей
+            {
+                Vector2Int enemyBase = runtimeModel.RoMap.Bases[
+                IsPlayerUnitBrain ? RuntimeModel.BotPlayerId : RuntimeModel.PlayerId];
+                result.Add(enemyBase);
+            }
+
             return result;
-            ///////////////////////////////////////
+
+        }
+
+        public override Vector2Int GetNextStep()
+        {
+            {
+                List<Vector2Int> targets = SelectTargets();
+
+                if (targets.Count > 0)
+                {
+                    foreach (Vector2Int targetPos in targets)
+                    {
+
+                        if (IsTargetInRange(targetPos))
+                        {
+                           
+                            return unit.Pos;
+                        }
+                        else
+                        {
+                            
+                            return CalcNextStepTowards(targetPos); // цель вне зоны досягаемости, вызываем метод для расчета следующего шага
+                        }
+                    }
+                }
+
+               
+                return unit.Pos;  // если целей нет, возвращаем текущую позицию юнита
+            }
+
+
         }
 
         public override void Update(float deltaTime, float time)
         {
             if (_overheated)
-            {              
+            {
                 _cooldownTime += Time.deltaTime;
-                float t = _cooldownTime / (OverheatCooldown/10);
+                float t = _cooldownTime / (OverheatCooldown / 10);
                 _temperature = Mathf.Lerp(OverheatTemperature, 0, t);
                 if (t >= 1)
                 {
@@ -55,7 +126,7 @@ namespace UnitBrains.Player
 
         private int GetTemperature()
         {
-            if(_overheated) return (int) OverheatTemperature;
+            if (_overheated) return (int)OverheatTemperature;
             else return (int)_temperature;
         }
 
