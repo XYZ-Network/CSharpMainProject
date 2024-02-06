0d6abc482619bf9ae70bdb614bdb88feea4544e5
Александр Кортуков <a.a.kortukov@gmail.com>
2024-02-06 22:33:54 +0300

    Missle

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using ActionGameFramework.Projectiles;
 using Model.Runtime.Projectiles;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace UnitBrains.Player
@@ -16,12 +18,20 @@ namespace UnitBrains.Player
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
             float overheatTemperature = OverheatTemperature;
-            ///////////////////////////////////////
-            // Homework 1.3 (1st block, 3rd module)
-            ///////////////////////////////////////           
-            var projectile = CreateProjectile(forTarget);
-            AddProjectileToList(projectile, intoList);
-            ///////////////////////////////////////
+
+            while (GetTemperature() <= overheatTemperature)
+            {
+                int temp = GetTemperature();
+
+                IncreaseTemperature();
+
+                for (int i = 0; i < temp; i++)
+                {
+                    var projectile = CreateProjectile(forTarget);
+                    AddProjectileToList(projectile, intoList);
+                }
+                break;
+            }
         }
 
         public override Vector2Int GetNextStep()
