335dda7bbcbd76e581734b46c4bfdf076e4a2df9
Rasul <rasul_bibalaev@mail.ru>
2024-02-15 14:58:12 +0300

    домашка  2
    изменил код согласно заданию

--- Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
+++ Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
@@ -29,12 +29,13 @@ namespace Model.Runtime.Projectiles
             ///////////////////////////////////////
             // Insert you code here
             ///////////////////////////////////////
-
-
+            float maxHeight;
+            maxHeight = totalDistance * 60 / 100; // 60 % от переменной
+            localHeight = maxHeight * (-(t * 2 - 1) * (t * 2 - 1)); // подставил формулу из домашки
             ///////////////////////////////////////
             // End of the code to insert
             ///////////////////////////////////////
-            
+
             Height = localHeight;
             if (time > StartTime + _timeToTarget)
                 Hit(_target);
