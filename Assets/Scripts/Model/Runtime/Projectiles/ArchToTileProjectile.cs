16493006e998398eb3f781604fef5c086ee27591
AlphaOmegaBravo <morov.vladik@gmail.com>
2024-02-23 22:10:47 +0500

    Domashka 2

--- Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
+++ Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
@@ -8,7 +8,7 @@ namespace Model.Runtime.Projectiles
         private readonly Vector2Int _target;
         private readonly float _timeToTarget;
         private readonly float _totalDistance;
-        
+
         public ArchToTileProjectile(Unit unit, Vector2Int target, int damage, Vector2Int startPoint) : base(damage, startPoint)
         {
             _target = target;
@@ -20,24 +20,20 @@ namespace Model.Runtime.Projectiles
         {
             float timeSinceStart = time - StartTime;
             float t = timeSinceStart / _timeToTarget;
-            
+
             Pos = Vector2.Lerp(StartPoint, _target, t);
-            
-            float localHeight = 0f;
-            float totalDistance = _totalDistance;
 
-            ///////////////////////////////////////
-            // Insert you code here
-            ///////////////////////////////////////
+            // Вычисление maxHeight
+            float maxHeight = 0.6f * _totalDistance;
 
+            // Вычисление localHeight по формуле
+            float localHeight = maxHeight * (-(t * 2 - 1) * (t * 2 - 1) + 1);
 
-            ///////////////////////////////////////
-            // End of the code to insert
-            ///////////////////////////////////////
-            
             Height = localHeight;
+
             if (time > StartTime + _timeToTarget)
                 Hit(_target);
         }
     }
 }
+
