afbff5d6b3408084aac19886db9535d7bb68084e
mish_hd <mihanik15@yandex.ru>
2024-03-01 01:46:26 +0300

    coment

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -11,7 +11,9 @@ public class EnterPoint : MonoBehaviour
     [SerializeField] private Settings _settings;
     [SerializeField] private Canvas _targetCanvas;
     private float _timeScale = 1;
-    
+    /// <summary>
+    /// привет, здесь был я
+    /// </summary>
     void Start()
     {
         Time.timeScale = _timeScale;
