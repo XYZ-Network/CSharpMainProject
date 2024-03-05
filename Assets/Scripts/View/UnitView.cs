97ebf810fbb03df7fa0b59772b87c64ded0f0038
nikiDotNET <noskovnikita@lenta.ru>
2024-03-05 12:09:31 +0300

    old comets

--- Assets/Scripts/View/UnitView.cs
+++ Assets/Scripts/View/UnitView.cs
@@ -1,5 +1,4 @@
 ﻿using Model.Runtime.ReadOnly;
-using UnitBrains.Pathfinding;
 using UnityEngine;
 
 namespace View
@@ -7,7 +6,6 @@ namespace View
     public class UnitView : MonoBehaviour
     {
         [SerializeField] private HealthBar _healthBar;
-        [SerializeField] private DebugPathOutput _debugPathOutput;
 
         public void UpdateState(IReadOnlyUnit model, Vector3 prevPosition)
         {
@@ -16,13 +14,6 @@ namespace View
             if (deltaPos != Vector3.zero)
             {
                 transform.rotation = Quaternion.LookRotation(deltaPos, Vector3.up);
-            }
-
-            if (_debugPathOutput != null &&
-                model.ActivePath != null &&
-                model.ActivePath?.EndPoint != _debugPathOutput.Path?.EndPoint)
-            {
-                _debugPathOutput.HighlightPath(model.ActivePath);
             }
         }
     }
