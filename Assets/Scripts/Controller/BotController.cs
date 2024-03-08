f62cc596913cd306fc059702ffec382b5d5158da
Razrabochik <162691297+Razrabochik@users.noreply.github.com>
2024-03-08 20:19:14 +0300

    controllers bot

--- Assets/Scripts/Controller/BotController.cs
+++ Assets/Scripts/Controller/BotController.cs
@@ -6,9 +6,9 @@ using Model;
 using Model.Config;
 using UnityEngine;
 using Utilities;
-//hello
-//How are you?
-//i'm fine
+
+// hello
+// how are you
 namespace Controller
 {
     public class BotController
