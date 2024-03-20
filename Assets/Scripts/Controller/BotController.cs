a01543cdc6997553ae8fe7bdd2e63f21d790fd16
mishppv <you@example.com>
2024-03-20 14:05:46 +0500

    commit

--- Assets/Scripts/Controller/BotController.cs
+++ Assets/Scripts/Controller/BotController.cs
@@ -6,9 +6,7 @@ using Model;
 using Model.Config;
 using UnityEngine;
 using Utilities;
-//hello
-//How are you?
-//i'm fine
+//where are you?
 namespace Controller
 {
     public class BotController
