865c8fdb99a2cd51515d07cd816a39d83516fee6
ckupid <alex01bar07@gmail.com>
2024-03-24 02:43:17 +0300

    8.1

---
+++ Assets/Scripts/UnitBrains/Player/ThirdUnitBrain.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnitBrains.Player;
+using UnityEngine;
+
+public class ThirdUnitBrain : DefaultPlayerUnitBrain
+   
+{
+    
+
+    public void IroncladBehemoth()
+    {
+        Console.WriteLine("Ironclad Behemoth");
+        return; 
+    }
+   
+
+  
+}
